82298085ed0f1a0c5c3659359e0d6fd326399630
Thasli <thaslinazeer27@gmail.com>
2026-03-23 12:59:17 +0530

    added url

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Thaslima@localhost/todo_db"
+DATABASE_URL = "sqlite:///./test.db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
