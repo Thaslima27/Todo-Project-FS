7059a3829d0fd9d1fb921615d86ca89739d27689
Thasli <thaslinazeer27@gmail.com>
2026-03-23 17:50:24 +0530

    changes

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,13 +1,8 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 
-load_dotenv()
+DATABASE_URL = "postgresql://postgres:Thaslima@localhost/todo_db"
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL"
-)
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
