1742db481aa0cbe5dd4f1b7ee01e9bda2b593f03
Thasli <thaslinazeer27@gmail.com>
2026-03-25 18:20:39 +0530

    commit changes

--- frontend/src/PrivateRoute.tsx
+++ frontend/src/PrivateRoute.tsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom"
 import { useAuthStore } from "./store/useAuthStore"
 
-function PrivateRoute({ children }: { children: JSX.Element }) {
+function PrivateRoute({ children } : { children : any}) {
   const token = useAuthStore((state) => state.token)
 
   return token ? children : <Navigate to="/login" />
